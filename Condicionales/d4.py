a62908791ce7693a499bd0156bc5f3c61f7d18a0
gianlucagranciclapasini <grancicg@gmail.com>
2020-07-01 23:06:23 -0300

    Ejer 4 con listas

--- Condicionales/d4.py
+++ Condicionales/d4.py
@@ -4,5 +4,21 @@
  Ingredientes Receta 2: Morrón y Cebolla.
 """
 
-receta_elegida = int(input("Ingresa la receta que deseas (1 o 2): "))
-if receta_elegida == "1":
+# Definiendo ingredientes en listas
+receta1 = ['Lentejas', 'Apio']
+receta2 = ['Morrón', 'Cebolla']
+comunes = ['Verduras', 'Cebolla']
+
+# Mostrando por pantalla las listas. El formato de salida sale como string con el metodo join
+print("1.-",", ".join(receta1))
+print("2.-",", ".join(receta2))
+rel = int(input("Selecciona tu receta: "))
+
+ex = int(input("Selecciona un ingrediente común: "))
+
+if rel > 2 or ex > 2:
+    print("Ingreso de datos incorrecto: ")
+else:
+    print("Su receta elegida es: ",
+
+#print("1.-", comunes[0],"\n2.-", comunes[1]
