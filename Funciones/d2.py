8e439943567e9959f30a0d8775ac8e0c87c26eba
grlap96 <grancicg@gmail.com>
2020-07-15 16:02:25 -0300

    Ejercicio terminado

--- Funciones/d2.py
+++ Funciones/d2.py
@@ -5,9 +5,31 @@ Si el primer número es menor que el segundo, debe devolver el nombre de la ciud
 Si ambos números son iguales, debe devolver el nombre de ambas.
 """
 
-def relacion(a, b):
-	"""
-	Acá debería recibir kwargs...
-	"""
+def relacion(datos):
+    """
+    Esta funcion recibe un diccionario con dos elementos como parámetro.
+    Luego toma los valores mas altos que haya con la funcion max().
+    Luego retorna la clave q corresponda a ese valor alto.
+    """
+    mas_alto = max(datos.values())
+    # En este caso usamos una list comprehension para escribir menos codigo.
+    # En definitiva retorna clave.
+    return ([clave for clave, valor in ciudades.items() if valor == mas_alto])
 
 
+# Pido datos al usuario, en este caso 2 veces
+# Ademas valido q no se repita ninguna key con while.
+ciudades = {}
+
+for i in range(2):
+    while True:
+        nombre_ciudad = input('Ingresa el nombre de la ciudad: ')
+        if nombre_ciudad in ciudades:
+            print("Ya existe ese nombre. Reintentar")
+        else:
+            break
+    ton_basura = float(input('Ingresa las toneladas de basura que se producen: '))
+    ciudades[nombre_ciudad] = ton_basura
+
+# Como lo que retorna la funcion es una lista, la pasamos a string y separamos con coma con la función join.
+print(", ".join(relacion(ciudades)))
