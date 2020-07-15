79144323d5b308b0dd4dcc1496f2e7f3a50d1411
grlap96 <grancicg@gmail.com>
2020-07-15 18:32:20 -0300

    función separar() agregada y corregida

--- Funciones/d3.py
+++ Funciones/d3.py
@@ -7,26 +7,34 @@ Tiene q devolver 2 listas ORDENADAS.
 Luego qué cantidad de ciudades han plantado más de 100 árboles.
 """
 
-def almacenar():
-    """
-    Esta funcion se encarga de pedir datos, validarlos y almacenarlos en un diccionario
-    """
-    datos = {}
-    while True:
-        nombre_ciudad = input('Ingresa el nombre de la ciudad: ')
-        while True:
-            if nombre_ciudad in datos:
-                print("Ya existe ese nombre. Reintentar")
-            else:
-                break
-        arboles_plantados = int(input('Ingresa la cantidad de arboles plantados: '))
+# Acá no voy a pedir datos al usuario, pero sería algo parecido como el Desafío 2 si quisiéramos implementarlo...
+ciudades = {
+    "Buenos Aires": 2500,
+    "Resistencia": 30,
+    "Corrientes Capital": 4000,
+    "Posadas": 6500,
+    "San Salvador de Jujuy": 10
+}
 
-        datos[nombre_ciudad] = arboles_plantados
-        
-        continuar = int(input("¿Deseas continuar ingresando datos?\n\t1- SI\n\t2- NO\nINGRESO: "))
-        if continuar == 2:
-            break
+
+def separar(datos):
+    """
+    Esta función recibe un diccionario como parámetro. Anexa valores mayores a una lista y menores a otra.
+    Además cuenta los mayores de 100. Finalmente, imprime por pantalla
+    """
+    mayores = []
+    menores = []
+    contar_mayores = 0
+
+    for clave, valor in datos.items():
+        if valor > 100:
+            mayores.append(clave)
+            contar_mayores += 1
+        else:
+            menores.append(clave)
     
-    return datos
+    print("Lista de ciudades que plantaron mas de 100 árboles: ", mayores)
+    print("Lista de ciudades que plantaron menos de 100 árboles: ", menores)
+    print("Cantidad de ciudades que plantaron mas de 100 árboles:", contar_mayores)
 
-
+separar(ciudades)
